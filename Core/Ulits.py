d52f0000b1d1982dd0f804ab405fd1f2e81f8395
pig <645531302@qq.com>
2020-02-02 01:49:06 +0800

    完善 取消订阅操作

--- Core/Ulits.py
+++ Core/Ulits.py
@@ -4,6 +4,9 @@
 import sqlite3
 import os
 import re
+import base64
+# from Crypto.Cipher import AES
+from Cryptodome.Cipher import AES
 
 # 验证邮箱是否合法
 def is_valid_email(email):
@@ -11,6 +14,48 @@ def is_valid_email(email):
         return True
     else :
         return False
+
+# AES对称加密
+class USE_AES:
+    """
+    AES
+    除了MODE_SIV模式key长度为：32, 48, or 64,
+    其余key长度为16, 24 or 32
+    详细见AES内部文档
+    CBC模式传入iv参数
+    本例使用常用的ECB模式
+    """
+
+    def __init__(self):
+        key = os.environ.get('AES_KEY','dwadawdijoi')
+        if len(key) > 32:
+            key = key[:32]
+        self.key = self.to_16(key)
+
+    def to_16(self, key):
+        """
+        转为16倍数的bytes数据
+        :param key:
+        :return:
+        """
+        key = bytes(key, encoding="utf8")
+        while len(key) % 16 != 0:
+            key += b'\0'
+            print("to_16")
+        return key  # 返回bytes
+
+    def aes(self):
+        return AES.new(self.key, AES.MODE_ECB) # 初始化加密器
+
+    def encrypt(self, text):
+        aes = self.aes()
+        return str(base64.encodebytes(aes.encrypt(self.to_16(text))),
+                   encoding='utf8').replace('\n', '')  # 加密
+
+    def decodebytes(self, text):
+        aes = self.aes()
+        return str(aes.decrypt(base64.decodebytes(bytes(
+            text, encoding='utf-8'))).rstrip(b'\0').decode("utf-8"))  # 解密
 
 # 数据库操作类
 class DBTool(object):
--- server.py
+++ server.py
@@ -15,35 +15,69 @@ class Resquest(BaseHTTPRequestHandler):
     def do_GET(self):
         
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type', 'text/html	')
         self.end_headers()
 
         if '?' in self.path :
-            queryString = urllib.parse.unquote(self.path.split('?',1)[1])
-            params = urllib.parse.parse_qs(queryString)        
+            # queryString = urllib.parse.unquote(self.path.split('?',1)[1])
+            # params = urllib.parse.parse_qs(queryString)        
+            params = dict(urllib.parse.parse_qsl(urllib.parse.urlsplit(self.path).query))
         else :
             params = {}
 
         # 添加邮箱
         if  '/add' in self.path:
-            if ('email' in params) == False :
-                data['msg'] = '请输入邮箱'
-            elif Ulits.is_valid_email(params['email'][0]) == False :
-                data['msg'] = '邮箱不合法'
-            else :
-                data['msg'] = 'ok'
-                data['code'] = 1
-                email = params['email'][0]
-                db = Ulits.DBTool()
-                dataIsAdd = db.executeQuery("SELECT * FROM emails WHERE email=?" ,(email,))
-                if len(dataIsAdd.fetchall()) == 0 :
-                    if db.executeUpdate("INSERT INTO emails (email, create_time) \
-                        VALUES (?,?)", [(email, time.time())]) == False :
-                        data['msg'] = '添加失败,请稍后重试'
-                        data['code'] = 0
-                db.close()
-            self.wfile.write(json.dumps(data).encode())
-            
+            try:
+                if ('email' in params) == False :
+                    data['msg'] = '请输入邮箱'
+                elif Ulits.is_valid_email(params['email']) == False :
+                    data['msg'] = '邮箱不合法'
+                else :
+                    data['msg'] = 'ok'
+                    data['code'] = 1
+                    email = params['email']
+                    db = Ulits.DBTool()
+                    dataIsAdd = db.executeQuery("SELECT * FROM emails WHERE email=?" ,(email,))
+                    dataIsAdd = dataIsAdd.fetchall()
+                    if len(dataIsAdd) == 0 :
+                        if db.executeUpdate("INSERT INTO emails (email, create_time) \
+                            VALUES (?,?)", [(email, time.time())]) == False :
+                            data['msg'] = '添加失败,请稍后重试'
+                            data['code'] = 0
+                    elif dataIsAdd[0][5] == 0 :
+                        if db.executeUpdate("UPDATE emails SET is_notice = 1 WHERE id =?", [(dataIsAdd[0][0],)]) == False :
+                            data['msg'] = '添加失败,请稍后重试'
+                            data['code'] = 0
+                    db.close()
+                self.wfile.write(json.dumps(data).encode())
+            except Exception as identifier:
+                self.send_error(404)
+
+        # 取消订阅
+        elif '/cancel' in self.path:
+            try:
+                if ('code' in params) :
+                    aes = Ulits.USE_AES()
+                    email = aes.decodebytes(params['code'])
+                    if Ulits.is_valid_email(email):
+                        db = Ulits.DBTool()
+                        dataIsAdd = db.executeQuery("SELECT * FROM emails WHERE email=?" ,(email,))
+                        dataIsAdd = dataIsAdd.fetchall()
+                        if len(dataIsAdd) > 0:
+                            db.executeUpdate("UPDATE emails SET is_notice = 0 WHERE id =?", [(dataIsAdd[0][0],)])
+                            data['code'] = 1
+                            data['msg'] = '操作成功'
+                        else:
+                            data['msg'] = '邮箱不存在'
+                        db.close()
+                    else :
+                        data['msg'] = '邮箱不合法'
+                else:
+                    data['msg'] = '标识符不存在'
+                self.wfile.write(json.dumps(data).encode())
+            except Exception as identifier:
+                self.send_error(404)
+                
 
 if __name__ == '__main__':
     server = HTTPServer(host, Resquest)
